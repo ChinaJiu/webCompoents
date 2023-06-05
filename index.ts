f324246740af4254254d1aed4f755b4dd54e8924
张烘玉 <1098029701@qq.com>
2023-06-05 14:00:49 +0800

    开发自定义组件,传值

---
+++ index.ts
@@ -0,0 +1,41 @@
+window.onload = () => {
+  class CustomDiv extends HTMLElement {
+    constructor() {
+      super()
+      const shadow = this.attachShadow({mode: "open"});
+      const tem = document.getElementById('custom-tem') as HTMLTemplateElement;
+      
+      shadow.appendChild(tem.content);
+      console.log(this.getAttr("name"));
+      console.log(this.getAttr("type"));
+    }
+
+    private getAttr(attr: string) {
+      return this.getAttribute(attr);
+    }
+
+    // 生命周期
+    // 插入
+    connectedCallback() {
+      console.log('Custom square element added to page.');
+    }
+
+    // 删除
+    disconnectedCallback() {
+      console.log('Custom square element removed from page.');
+    }
+    
+    // 移动
+    adoptedCallback() {
+      console.log('Custom square element moved to new page.');
+    }
+
+    // 增加、删除、修改自身属性时，被调用。
+    attributeChangedCallback() {
+      console.log('Custom square element attributes changed.');
+    }
+    
+
+  }
+  window.customElements.define('custom-div', CustomDiv);
+}
